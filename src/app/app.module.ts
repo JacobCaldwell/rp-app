94b81ebb496789b2d0c84b433d3fd180a5076c94
Jacob S. Caldwell <jacob.caldwell@us.abb.com>
2018-10-02 13:00:34 -0500

    ABB Header Added - Stable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
